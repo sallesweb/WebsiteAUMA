8dcfaebf6e7ebf5e0507a7a0bf3a1eaa33f6308e
Felipe Salles <felipesalles.ti@gmail.com>
2020-07-16 00:36:04 -0300

    Versão inicial

---
+++ js/personalizado.js
@@ -0,0 +1,18 @@
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.card-principios, .video', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '20px'
+});
+
+sr.reveal('.obj-text, .autismo-text-left, .autismo-img-left, .auma-text-left, .auma-list-left', {
+    duration: 1000,
+    origin: 'left',
+    distance: '20px'
+});
+
+sr.reveal('.obj-img, .autismo-img-right, .autismo-text-right, .auma-img-right, .auma-list-right', {
+    duration: 1500,
+    origin: 'right',
+    distance: '20px'
+});
